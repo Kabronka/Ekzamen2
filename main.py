a48f6a9a79b78414fe8d25a8082b1aa2682a53cc
Kabronka <alexsavushkin2006@gmail.com>
2024-06-26 19:05:21 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#создаем метод print_string_n_times
 def print_string_n_times(string, n):
     if n > 0:
         print(string)
@@ -6,4 +7,5 @@ def print_string_n_times(string, n):
 input_string = input("Введите строку: ")
 num_times = int(input("Введите количество повторений: "))
 
+#вызываем метод print_string_n_times
 print_string_n_times(input_string, num_times)
